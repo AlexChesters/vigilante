5a06a36011fac515d11362608f49522f7da5e340
Alex Chesters <alex.chesters@bbc.co.uk>
2018-10-06 14:28:57 +0100

    add concept of route handlers

--- example/server.js
+++ example/server.js
@@ -4,3 +4,13 @@ vigilante.start(
   8080,
   () => { console.log('vigilante example server is running') }
 )
+
+vigilante.addRouteHandlers([
+  {
+    method: 'GET',
+    path: '/foo',
+    handler: (req, res) => {
+      res.write('<h1>Hello, world!</h1>')
+    }
+  }
+])
