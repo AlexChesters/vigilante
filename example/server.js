6380c668fac74f9291a68353903a3a2468c6e273
Alex Chesters <alex.chesters@bbc.co.uk>
2018-10-06 14:48:34 +0100

    Add support for async handlers

--- example/server.js
+++ example/server.js
@@ -1,11 +1,22 @@
 const vigilante = require('../index')()
+const fetch = require('node-fetch')
 
 vigilante.addRouteHandlers([
   {
     method: 'GET',
-    path: '/foo',
-    handle: (req, res) => {
+    path: '/',
+    handle: (_, res) => {
       res.write('<h1>Hello, world!</h1>')
+    }
+  },
+  {
+    method: 'GET',
+    path: '/foo',
+    handle: async (_, res) => {
+      const response = await fetch('https://www.reddit.com/hot.json')
+      const data = await response.json()
+      const foo = data.data.children.slice(0, 2)
+      res.write(JSON.stringify(foo))
     }
   }
 ])
